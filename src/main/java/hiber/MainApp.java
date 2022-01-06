92bf31a803c10ad656836c360577453ec96c8438
DeniskaStar <Popcorn2008@mail.ru>
2022-01-06 15:11:31 +0700

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,45 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("nissan", 5)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("toyota", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("mazda", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("lexus", 4)));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("CarModel = " +user.getCar().getModel());
+         System.out.println("CarSeries = " +user.getCar().getSeries());
+         System.out.println();
+      }
+
+      List <User> userByCar = userService.getUser("nissan", 5);
+      for (User user: userByCar){
+         System.out.println("Владельцем машины по выбранным критериям:");
+         System.out.println("CarModel: " + user.getCar().getModel());
+         System.out.println("CarSeries: " + user.getCar().getSeries());
+         System.out.println("Является пользователь: " + user);
+      }
+
+      context.close();
+   }
+}
